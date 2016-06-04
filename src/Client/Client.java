aa22cf68c6fb157543731652e56ea8a74de81eee
nogna <nogna@hotmail.se>
2016-06-04 23:29:59 +0200

    Fönstret finns där inget i

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -42,7 +42,7 @@ public class Client extends JFrame implements ActionListener{
     static private Object SERVER_RESPONSE = null;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        //setUpWindow();
+        setUpWindow();
         setUpToServer();
 
     }
